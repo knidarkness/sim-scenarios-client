9ce54914746e783723e8b7583d40c9a0955f6778
knidarkness <knidarkness@gmail.com>
2026-02-15 23:56:40 +0100

    refactor: remove obsolete Electron main, preload, and simconnect files

--- dist-electron/preload.cjs
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const electron_1 = require("electron");
-electron_1.contextBridge.exposeInMainWorld("simconnect", {
-    setLogoLightOn: () => electron_1.ipcRenderer.invoke("simconnect:setLogoLightOn"),
-    setScenarios: (scenario) => electron_1.ipcRenderer.invoke("simconnect:setScenarios", scenario),
-    clearScenarios: () => electron_1.ipcRenderer.invoke("simconnect:clearScenarios"),
-});
