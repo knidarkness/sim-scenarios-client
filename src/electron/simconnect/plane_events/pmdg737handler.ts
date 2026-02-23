63b967ccd877c9f3b7a35b9477a9c30d8faf8416
knidarkness <knidarkness@gmail.com>
2026-02-23 18:56:39 +0100

    chore: simplify simevent maps for pmdg planes

--- src/electron/simconnect/plane_events/pmdg737handler.ts
+++ src/electron/simconnect/plane_events/pmdg737handler.ts
@@ -1,33 +1,101 @@
 import { SimConnectConnection } from "node-simconnect";
-import {
-  PMDG_737_EVENT_MAP,
-} from "../types";
+import { EventMapEntry } from "../types";
 import { PMDGBaseCommandHandler } from "./pmdgBaseHandler";
 
 export class PMDG737CommandHandler extends PMDGBaseCommandHandler {
+  private static readonly THIRD_PARTY_EVENT_ID_MIN = 69632;
+
+  private static readonly EVENT_MAP: Record<string, EventMapEntry> = {
+    CDU_R_L1: {
+      clientEventId: 2001,
+      simEventName: `#${PMDG737CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 606}`,
+    },
+    CDU_R_L2: {
+      clientEventId: 2002,
+      simEventName: `#${PMDG737CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 607}`,
+    },
+    CDU_R_L3: {
+      clientEventId: 2003,
+      simEventName: `#${PMDG737CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 608}`,
+    },
+    CDU_R_L4: {
+      clientEventId: 2004,
+      simEventName: `#${PMDG737CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 609}`,
+    },
+    CDU_R_L5: {
+      clientEventId: 2005,
+      simEventName: `#${PMDG737CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 610}`,
+    },
+    CDU_R_L6: {
+      clientEventId: 2006,
+      simEventName: `#${PMDG737CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 611}`,
+    },
+    CDU_R_R1: {
+      clientEventId: 2007,
+      simEventName: `#${PMDG737CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 612}`,
+    },
+    CDU_R_R2: {
+      clientEventId: 2008,
+      simEventName: `#${PMDG737CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 613}`,
+    },
+    CDU_R_R3: {
+      clientEventId: 2009,
+      simEventName: `#${PMDG737CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 614}`,
+    },
+    CDU_R_R4: {
+      clientEventId: 2010,
+      simEventName: `#${PMDG737CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 615}`,
+    },
+    CDU_R_R5: {
+      clientEventId: 2011,
+      simEventName: `#${PMDG737CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 616}`,
+    },
+    CDU_R_R6: {
+      clientEventId: 2012,
+      simEventName: `#${PMDG737CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 617}`,
+    },
+    CDU_R_EXEC: {
+      clientEventId: 2013,
+      simEventName: `#${PMDG737CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 628}`,
+    },
+    CDU_R_MENU: {
+      clientEventId: 2014,
+      simEventName: `#${PMDG737CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 623}`,
+    },
+    CDU_R_PREV_PAGE: {
+      clientEventId: 2015,
+      simEventName: `#${PMDG737CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 631}`,
+    },
+    CDU_R_NEXT_PAGE: {
+      clientEventId: 2016,
+      simEventName: `#${PMDG737CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 632}`,
+    },
+  };
 
   constructor(simConnectConnection: SimConnectConnection) {
+    const eventMap = PMDG737CommandHandler.EVENT_MAP;
+
     super(simConnectConnection, {
       aircraft: "PMDG73X",
-      eventMap: PMDG_737_EVENT_MAP,
+      eventMap,
       lskRows: [
-        PMDG_737_EVENT_MAP.CDU_R_L1,
-        PMDG_737_EVENT_MAP.CDU_R_L2,
-        PMDG_737_EVENT_MAP.CDU_R_L3,
-        PMDG_737_EVENT_MAP.CDU_R_L4,
-        PMDG_737_EVENT_MAP.CDU_R_L5,
+        eventMap.CDU_R_L1,
+        eventMap.CDU_R_L2,
+        eventMap.CDU_R_L3,
+        eventMap.CDU_R_L4,
+        eventMap.CDU_R_L5,
       ],
       navigateToFaults: [
-        PMDG_737_EVENT_MAP.CDU_R_MENU,
-        PMDG_737_EVENT_MAP.CDU_R_R4,
-        PMDG_737_EVENT_MAP.CDU_R_L1,
-        PMDG_737_EVENT_MAP.CDU_R_L3,
+        eventMap.CDU_R_MENU,
+        eventMap.CDU_R_R4,
+        eventMap.CDU_R_L1,
+        eventMap.CDU_R_L3,
       ],
-      nextPage: PMDG_737_EVENT_MAP.CDU_R_NEXT_PAGE,
-      selectProgrammed: PMDG_737_EVENT_MAP.CDU_R_L1,
-      activate: PMDG_737_EVENT_MAP.CDU_R_L1,
-      execute: PMDG_737_EVENT_MAP.CDU_R_EXEC,
-      exitMenu: PMDG_737_EVENT_MAP.CDU_R_MENU,
+      nextPage: eventMap.CDU_R_NEXT_PAGE,
+      selectProgrammed: eventMap.CDU_R_L1,
+      activate: eventMap.CDU_R_L1,
+      execute: eventMap.CDU_R_EXEC,
+      exitMenu: eventMap.CDU_R_MENU,
       faultsPerPage: 5,
       handlerName: "PMDG737CommandHandler",
     });
--- src/electron/simconnect/plane_events/pmdg777handler.ts
+++ src/electron/simconnect/plane_events/pmdg777handler.ts
@@ -1,33 +1,106 @@
 import { SimConnectConnection } from "node-simconnect";
-import {
-  PMDG_777_EVENT_MAP_CDU_C,
-} from "../types";
+import { EventMapEntry } from "../types";
 import { PMDGBaseCommandHandler } from "./pmdgBaseHandler";
 
 export class PMDG777CommandHandler extends PMDGBaseCommandHandler {
+  private static readonly THIRD_PARTY_EVENT_ID_MIN = 69632;
+  private static readonly CDU_C_L1 =
+    PMDG777CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 653;
+  private static readonly CDU_C_EVENT_OFFSET =
+    PMDG777CommandHandler.CDU_C_L1 -
+    (PMDG777CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 328);
+
+  private static readonly EVENT_MAP: Record<string, EventMapEntry> = {
+    CDU_C_L1: {
+      clientEventId: 3101,
+      simEventName: `#${PMDG777CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 653}`,
+    },
+    CDU_C_L2: {
+      clientEventId: 3102,
+      simEventName: `#${PMDG777CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 329 + PMDG777CommandHandler.CDU_C_EVENT_OFFSET}`,
+    },
+    CDU_C_L3: {
+      clientEventId: 3103,
+      simEventName: `#${PMDG777CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 330 + PMDG777CommandHandler.CDU_C_EVENT_OFFSET}`,
+    },
+    CDU_C_L4: {
+      clientEventId: 3104,
+      simEventName: `#${PMDG777CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 331 + PMDG777CommandHandler.CDU_C_EVENT_OFFSET}`,
+    },
+    CDU_C_L5: {
+      clientEventId: 3105,
+      simEventName: `#${PMDG777CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 332 + PMDG777CommandHandler.CDU_C_EVENT_OFFSET}`,
+    },
+    CDU_C_L6: {
+      clientEventId: 3106,
+      simEventName: `#${PMDG777CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 333 + PMDG777CommandHandler.CDU_C_EVENT_OFFSET}`,
+    },
+    CDU_C_R1: {
+      clientEventId: 3107,
+      simEventName: `#${PMDG777CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 334 + PMDG777CommandHandler.CDU_C_EVENT_OFFSET}`,
+    },
+    CDU_C_R2: {
+      clientEventId: 3108,
+      simEventName: `#${PMDG777CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 335 + PMDG777CommandHandler.CDU_C_EVENT_OFFSET}`,
+    },
+    CDU_C_R3: {
+      clientEventId: 3109,
+      simEventName: `#${PMDG777CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 336 + PMDG777CommandHandler.CDU_C_EVENT_OFFSET}`,
+    },
+    CDU_C_R4: {
+      clientEventId: 3110,
+      simEventName: `#${PMDG777CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 337 + PMDG777CommandHandler.CDU_C_EVENT_OFFSET}`,
+    },
+    CDU_C_R5: {
+      clientEventId: 3111,
+      simEventName: `#${PMDG777CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 338 + PMDG777CommandHandler.CDU_C_EVENT_OFFSET}`,
+    },
+    CDU_C_R6: {
+      clientEventId: 3112,
+      simEventName: `#${PMDG777CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 339 + PMDG777CommandHandler.CDU_C_EVENT_OFFSET}`,
+    },
+    CDU_C_EXEC: {
+      clientEventId: 3113,
+      simEventName: `#${PMDG777CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 349 + PMDG777CommandHandler.CDU_C_EVENT_OFFSET}`,
+    },
+    CDU_C_MENU: {
+      clientEventId: 3114,
+      simEventName: `#${PMDG777CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 350 + PMDG777CommandHandler.CDU_C_EVENT_OFFSET}`,
+    },
+    CDU_C_PREV_PAGE: {
+      clientEventId: 3115,
+      simEventName: `#${PMDG777CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 352 + PMDG777CommandHandler.CDU_C_EVENT_OFFSET}`,
+    },
+    CDU_C_NEXT_PAGE: {
+      clientEventId: 3116,
+      simEventName: `#${PMDG777CommandHandler.THIRD_PARTY_EVENT_ID_MIN + 353 + PMDG777CommandHandler.CDU_C_EVENT_OFFSET}`,
+    },
+  };
 
   constructor(simConnectConnection: SimConnectConnection) {
+    const eventMap = PMDG777CommandHandler.EVENT_MAP;
+
     super(simConnectConnection, {
       aircraft: "PMDG777",
-      eventMap: PMDG_777_EVENT_MAP_CDU_C,
+      eventMap,
       lskRows: [
-        PMDG_777_EVENT_MAP_CDU_C.CDU_C_L1,
-        PMDG_777_EVENT_MAP_CDU_C.CDU_C_L2,
-        PMDG_777_EVENT_MAP_CDU_C.CDU_C_L3,
-        PMDG_777_EVENT_MAP_CDU_C.CDU_C_L4,
-        PMDG_777_EVENT_MAP_CDU_C.CDU_C_L5,
+        eventMap.CDU_C_L1,
+        eventMap.CDU_C_L2,
+        eventMap.CDU_C_L3,
+        eventMap.CDU_C_L4,
+        eventMap.CDU_C_L5,
       ],
       navigateToFaults: [
-        PMDG_777_EVENT_MAP_CDU_C.CDU_C_MENU,
-        PMDG_777_EVENT_MAP_CDU_C.CDU_C_R5,
-        PMDG_777_EVENT_MAP_CDU_C.CDU_C_L1,
-        PMDG_777_EVENT_MAP_CDU_C.CDU_C_L3,
+        eventMap.CDU_C_MENU,
+        eventMap.CDU_C_R5,
+        eventMap.CDU_C_L1,
+        eventMap.CDU_C_L3,
       ],
-      nextPage: PMDG_777_EVENT_MAP_CDU_C.CDU_C_NEXT_PAGE,
-      selectProgrammed: PMDG_777_EVENT_MAP_CDU_C.CDU_C_L1,
-      activate: PMDG_777_EVENT_MAP_CDU_C.CDU_C_L1,
-      execute: PMDG_777_EVENT_MAP_CDU_C.CDU_C_EXEC,
-      exitMenu: PMDG_777_EVENT_MAP_CDU_C.CDU_C_MENU,
+      nextPage: eventMap.CDU_C_NEXT_PAGE,
+      selectProgrammed: eventMap.CDU_C_L1,
+      activate: eventMap.CDU_C_L1,
+      execute: eventMap.CDU_C_EXEC,
+      exitMenu: eventMap.CDU_C_MENU,
       faultsPerPage: 5,
       handlerName: "PMDG777CommandHandler",
     });
